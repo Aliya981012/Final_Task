37ab3a593355fa3898269d0a2ca24ed97e808b42
Aliya <autyabaeva@mail.ru>
2023-02-06 23:28:44 +0400

    End

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿void Final_Task()
+{
+    Console.WriteLine("Hello! First array:");
+    string[] first = {"very","cool","lessons","can","not","decide","what","to","choose","next"};
+    int size = first.Length;
+    PrintArray(first);
+    int count = 0;
+    for(int i = 0; i < size; i++)
+    {
+        if (first[i].Length <= 3) count++;
+    }
+    string[] second = new string[count];
+    for(int i = 0, j = 0; i < size; i++)
+    {
+        if (first[i].Length < 4)
+        {
+            second[j] = first[i];
+            j++;
+        } 
+    }
+    Console.WriteLine();
+    Console.WriteLine("The second array is:");
+    PrintArray(second);
+
+
+}
+void PrintArray(string[] array)
+{
+    int size = array.Length;
+    Console.Write("[ ");
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+    Console.Write("]");
+}
+
+
+Final_Task();
